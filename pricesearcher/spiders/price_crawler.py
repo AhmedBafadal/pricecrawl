87c6ed7d38c760eba303eb1733bc7f28247c9b6d
AhmedBafadal <a.m.bafadal@gmail.com>
2019-11-29 17:00:40 +0000

    Cleaned

--- pricesearcher/spiders/price_crawler.py
+++ pricesearcher/spiders/price_crawler.py
@@ -1,14 +1,13 @@
 
 from scrapy.spiders import Request, CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-import logging
 from scrapy import Request
 from tldextract import extract
 from urllib.robotparser import RobotFileParser
-from ..items import PricesearcherItem
 
 class PriceCrawlerSpider(CrawlSpider):
     name = "pricecrawler"
+    # Read from urls.txt file
     with open("urls.txt", "rt") as f:
         start_urls = [url.strip() for url in f.readlines()]
 
@@ -18,7 +17,7 @@ class PriceCrawlerSpider(CrawlSpider):
 
     def parse(self, response):
         le = LinkExtractor(unique=True)
-
+        # Use robot file parser to check if links extracted are allowed
         rp = RobotFileParser()
         BASE_URL = response.request.url
         DOMAIN = extract(BASE_URL).domain
@@ -28,8 +27,10 @@ class PriceCrawlerSpider(CrawlSpider):
         # Extract all the links
         all_links = le.extract_links(response)
         for link in all_links:
+            # Ensure same domain
             if extract(link.url).domain == DOMAIN:
                 if rp.can_fetch("*", link.url):
+                    # Enforcing unique urls 
                     if link.url not in self.unique_urls:
                         self.write_to_file(link.url, DOMAIN)
                         self.unique_urls.add(link.url)
@@ -44,8 +45,4 @@ class PriceCrawlerSpider(CrawlSpider):
 
                 
 
-# deliverables
-# tld check if url is domain, filter if robots.txt - D
-# concurrent_requests in settings set to 2 - D
-# download_delay in settings set to 1 sec - D
 
